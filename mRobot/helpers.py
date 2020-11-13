6232fa74887fcbccb2309b1c74a4da9484cf2fe3
Murilo Oliveira <murilo.oliveira7@gmail.com>
2020-11-13 10:43:48 +1000

    Commit 1.04
    1 - New Class to SSH connections

--- mRobot/helpers.py
+++ mRobot/helpers.py
@@ -1,7 +1,10 @@
+import time
+import random
 from pythonping import ping
 import urllib.request
-import random
-import time
+import smtplib
+import paramiko
+import os
 
 class Pingc: # Ping Class to generate ICMP traffic
     def __init__(self, ip):
@@ -11,7 +14,7 @@ class Pingc: # Ping Class to generate ICMP traffic
         print('mRobot will ping ' + self.ip)
         time.sleep(random.randint(3, 12)) # script will wait between 3 and 12 seconds
         ping(self.ip, count=random.randint(1, 73), verbose=True) # ping function will ping between 5 and 73 times the ip
-        print('mRobot has finished the ping\n')
+        print('mRobot has finished the ping\n') # Print confirmation
 
 class Httpc: #HTTP Class to generate HTTP traffic
     def __init__(self, http_url):
@@ -29,7 +32,7 @@ class Httpc: #HTTP Class to generate HTTP traffic
         resp = urllib.request.urlopen(req)  # acess the url using the request above
 
         if resp != 0:  # Check if the URL was acessed
-            print(self.http_url + " was accessed!\n")
+            print(self.http_url + " was accessed!\n") # Print confirmation
 
 class Emailc:
     def __init__(self, semail, spasswd, remail):
@@ -38,7 +41,7 @@ class Emailc:
         self.remail = remail # Receiver of the message
 
     def start_email_send(self):
-        print('mRobot will send an email to ' + self.email)
+        print('mRobot will send an email to ' + self.remail)
         time.sleep(random.randint(3, 12))  # script will wait between 3 and 12 seconds
 
         server = smtplib.SMTP('smtp.gmail.com', 587)  # SMTP configuration of the server
@@ -47,8 +50,59 @@ class Emailc:
 
         localtime = time.asctime(time.localtime(time.time())) # Getting formatted time
 
-        message_body = "This email was sent from a python code on " # Message that will be sent
+        message_body = This email was sent from a python code on ' # Message that will be sent
         message_body = message_body + localtime
         server.sendmail(self.semail, self.remail, message_body)  # Sending the message
         print(self.remail + ' received an email\n')  # Print confirmation
         server.quit()  # Close connection
+
+class Sshc:
+    def __init__(self, ip, user, passwd):
+        self.ip = ip # IP of the ssh server
+        self.user = user # User of the ssh server
+        self.passwd = passwd # User's password of the ssh server
+
+    def start_ssh_connection(self):
+        print('mRobot will ssh to ' + self.ip)
+        time.sleep(random.randint(3, 12))  # script will wait between 3 and 12 seconds
+
+        session = paramiko.SSHClient()  # Importing the function
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Accept any new host connection
+
+        session.connect(self.ip, username=self.user, password=self.passwd)  # Openning connection
+
+        # Commands
+        stdin, stdout, stderr = session.exec_command('ls', 'lsls')
+        lines = stdout.readlines()
+        print(lines)
+
+        session.close()  # Closing connection
+
+    def start_ssh_connection_mcommands(self):
+        print('mRobot will ssh to ' + self.ip)
+        time.sleep(random.randint(3, 12))  # script will wait between 3 and 12 seconds
+
+        session = paramiko.SSHClient()  # Importing the function
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Accept any new host connection
+
+        session.connect(self.ip, username=self.user, password=self.passwd)  # Openning connection
+
+        channel = session.invoke_shell()
+        stdin = channel.makefile('wb')
+        stdout = channel.makefile('rb')
+
+        # Commands
+        stdin.write('''
+        cd /home/m/Documents/
+        mkdir python_access
+        cd python_access
+        date >> access_log
+        exit
+        ''')
+        print (stdout.read())
+
+        stdout.close()
+        stdin.close()
+        session.close() # Closing connection
+
+        print (self.ip + ' session was closed') # Print confirmation
--- mRobot/mRobot.py
+++ mRobot/mRobot.py
@@ -1,6 +1,7 @@
 from helpers import Pingc
 from helpers import Httpc
 from helpers import Emailc
+from helpers import Sshc
 import time
 
 print('starting...')
@@ -13,11 +14,14 @@ print('|_| |_| |_|_| \_\___/|_.__/ \___/ \__|\n\n')
 
 time.sleep(10)
 
-p1 = Pingc('8.8.8.8)
+p1 = Pingc('8.8.8.8')
 p1.start_ping()
 
 h1 = Httpc('http://google.com')
 h1.start_http_connection()
 
-e1 = Emailc('seiya.ubuntu@gmail.com','KxouirXq.rt.8WR' , 'murilo.oliveira7@hotmail.com')
+e1 = Emailc('seiya.ubuntu@gmail.com', 'passwd', 'murilo.oliveira7@hotmail.com')
 e1.start_email_send()
+
+s1 = Sshc('M4G0', 'm', 'passwd')
+s1.start_ssh_connection()
